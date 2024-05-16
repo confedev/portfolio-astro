9ccea5a3e07cca0c7bc435cbe30f271d5c92aaa1
Felipe Contreras <confedev@gmail.com>
2024-05-16 12:14:35 -0400

    feat: improve MarqueeTech component with animation and styling

--- src/data/data.ts
+++ src/data/data.ts
@@ -84,4 +84,120 @@ export const TECHNOLOGIES = [
     name: "Tailwind CSS",
     score: 4,
   },
+  {
+    name: "React",
+    score: 4,
+  },
+  {
+    name: "Node.js",
+    score: 4,
+  },
+  {
+    name: "Express",
+    score: 4,
+  },
+  {
+    name: "MongoDB",
+    score: 4,
+  },
+  {
+    name: "PostgreSQL",
+    score: 4,
+  },
+  {
+    name: "GraphQL",
+    score: 4,
+  },
+  {
+    name: "Apollo",
+    score: 4,
+  },
+  {
+    name: "Prisma",
+    score: 4,
+  },
+  {
+    name: "TypeScript",
+    score: 4,
+  },
+  {
+    name: "JavaScript",
+    score: 4,
+  },
+  {
+    name: "HTML",
+    score: 4,
+  },
+  {
+    name: "CSS",
+    score: 4,
+  },
+  {
+    name: "SASS",
+    score: 4,
+  },
+  {
+    name: "Styled Components",
+    score: 4,
+  },
+  {
+    name: "Redux",
+    score: 4,
+  },
+  {
+    name: "MobX",
+    score: 4,
+  },
+  {
+    name: "Jest",
+    score: 4,
+  },
+  {
+    name: "Testing Library",
+    score: 4,
+  },
+  {
+    name: "Cypress",
+    score: 4,
+  },
+  {
+    name: "Storybook",
+    score: 4,
+  },
+  {
+    name: "Figma",
+    score: 4,
+  },
+  {
+    name: "Adobe XD",
+    score: 4,
+  },
+  {
+    name: "Photoshop",
+    score: 4,
+  },
+  {
+    name: "Illustrator",
+    score: 4,
+  },
+  {
+    name: "InDesign",
+    score: 4,
+  },
+  {
+    name: "Premiere Pro",
+    score: 4,
+  },
+  {
+    name: "After Effects",
+    score: 4,
+  },
+  {
+    name: "Blender",
+    score: 4,
+  },
+  {
+    name: "Unity",
+    score: 4,
+  },
 ];
